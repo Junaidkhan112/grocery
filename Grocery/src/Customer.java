9b490a8c940d15a9c484518cafc2376d75bbfefe
junaid <jk8kvv@gmail.com>
2019-08-29 00:24:27 -0500

    First commit

---
+++ Grocery/src/Customer.java
@@ -0,0 +1,29 @@
+
+public class Customer {
+
+	public static void main(String[] args) {
+		
+		 int orderID;
+	     String name;
+	     String adress;
+	     String ordertotal;
+	     
+	     orderID=123;
+	     name="Junaid";
+	     adress="123 devon ave";
+	     ordertotal="$75";
+	     
+	     System.out.println("Welcome  ");
+	     System.out.println("order ID: "+orderID);
+	     System.out.println("name: "+ name );
+	     System.out.println("adress: "+adress);
+	     System.out.println("ordertotal" +ordertotal);
+	     System.out.println("Thank you");
+	}
+     
+    
+    
+    
+    
+     
+} 
